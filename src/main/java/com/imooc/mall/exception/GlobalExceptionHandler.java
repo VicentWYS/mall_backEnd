668bd3a607ea556e7970001cfdafe655d9cef67e
wys <15835431945@163.com>
2025-02-04 18:37:45 +0800

    feat | 注册：添加将Service层的异常转换为ApiRestResponse对象的功能

---
+++ src/main/java/com/imooc/mall/exception/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.imooc.mall.exception;
+
+import com.imooc.mall.common.ApiRestResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 当Controller中出现错误时，可直接返回ApiRestResponse对象的error()；
+ * 但是，当Service层的Impl方法中要返回错误情况时，它将抛出自定义的异常类ImoocMallException(继承自Exception)；
+ * 因此，此处需要将这个异常类也包装为ApiRestResponse对象的error()
+ */
+@ControllerAdvice // 拦截异常
+public class GlobalExceptionHandler {
+    private final Logger log = (Logger) LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 系统异常
+     *
+     * @param e 系统异常类对象 Exception
+     * @return ApiRestResponse对象
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody // 返回json格式
+    public Object handleException(Exception e) {
+        // 打印为日志
+        log.error("Default Exception: ", e); // 此处不需研究原理，直接使用即可
+
+        // 将异常包装为ApiRestResponse对象
+        return ApiRestResponse.error(ImoocMallExceptionEnum.SYSTEM_ERROR);
+    }
+
+    /**
+     * 业务异常
+     *
+     * @param e 业务异常类对象 ImoocMallException
+     * @return ApiRestResponse对象
+     */
+    @ExceptionHandler(ImoocMallException.class)
+    @ResponseBody // 返回json格式
+    public Object handleImoocMallException(ImoocMallException e) {
+        // 打印为日志
+        log.error("ImoocMall Exception: ", e); // 此处不需研究原理，直接使用即可
+
+        // 将异常包装为ApiRestResponse对象
+        return ApiRestResponse.error(e.getCode(), e.getMessage()); // 可返回不同业务异常情况
+    }
+}
